0c4883856271ee36ca134eb29557c7205af44b6e
79064 <79064@MSI>
2021-11-29 00:27:45 +0300

    Добавьте файлы проекта.

---
+++ Lab11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab11_bibl_;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation=new Equation();
+            equation.K = 8;
+            equation.b = 6;
+            equation.Root();
+            Console.ReadKey();
+        }
+    }
+}
